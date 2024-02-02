a55916ee3e6d9e0396935328cf3c50a11e78875b
Mitchel Inaju <mitchelinaju@punch.agency>
2024-02-02 06:02:16 +0100

    worked on the part before the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import PNI_Mic from './assets/PNI-mic.png';
 import CarouselContainer from './components/carousel-container.jsx';
 import Hero from './containers/hero.jsx';
 
@@ -28,11 +29,49 @@ export default function App() {
           <div className="flex h-[50vh]"></div>
         </div>
       </div>
-      <div>
-        <CarouselContainer data={carouselItemData[0]} />
-      </div>
-      <div>
-        <CarouselContainer data={carouselItemData[1]} />
+      <div className="bg-[#E8E6E2]">
+        <div>
+          <CarouselContainer data={carouselItemData[0]} />
+        </div>
+        <div>
+          <CarouselContainer data={carouselItemData[1]} />
+        </div>
+
+        <div className=" relative mx-auto flex flex-col items-center justify-center py-20">
+          {/* <div className="bg-white top-10 left-10 w-[450px] h-[300px] -z-1000 absolute"></div> */}
+          <div className=" absolute left-[6.6%] ">
+            <div className="blend-container max-w-[600px] text-[3.5rem] font-bold leading-[5rem]">
+              A Fam<span>ily Bound </span>Togeth<span>er With Love</span>
+            </div>
+            <div className="blend-container mt-4 max-w-[500px] text-[1.2rem]">
+              We seek to promo<span>te brotherly love and affection as we</span>{' '}
+              fellowship. Serving{' '}
+              <span>God together with one passion, and</span> knit in love as 
+              famil<span>y.  Put a picture of a lovely setting or</span> people
+              smiling sha<span>at a meeting</span>
+            </div>
+          </div>
+          <div className="h-[416px] w-[826px] border-[10px]">
+            <img src={PNI_Mic} />
+          </div>
+        </div>
+
+        <div className="border-[4px] border-white bg-[#F78E00] p-6 text-white w-[800px] mx-auto flex flex-col items-center justify-center ">
+          <div className="mx-auto max-w-[697px] text-[1.5rem] ">
+            <div>
+              Join Us Every Sunday{' '}
+              <span className="mx-2 font-semibold">
+                9:00am, 10:45am, & 4:30pm
+              </span>
+            </div>
+            <div>
+              Address
+              <span className="mx-2 font-semibold">
+                11832 S. Warner Elliot LoopPhoenix, AZ 85044
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
